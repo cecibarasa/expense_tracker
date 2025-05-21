a234033cff7593158c64ba2c1d732ecd58db178a
cecibarasa <cecinbarasa@gmail.com>
2025-05-21 07:04:45 +0300

    depoyment

--- expense/settings.py
+++ expense/settings.py
@@ -110,6 +110,11 @@ WSGI_APPLICATION = 'expense.wsgi.application'
 DATABASES = {
     'default': {  # Add the 'default' key
         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
